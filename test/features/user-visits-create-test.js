b3356d34b9108f7f2f43cf20cf29efd98bffd53a
SalmonKing72 <madichjm@gmail.com>
2019-03-18 22:14:38 -0400

    feature level testing complete, red server level testing for handling create new item

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -2,4 +2,18 @@ const {assert} = require('chai');
 const {buildItemObject} = require('../test-utils');
 
 // Add your tests below:
+describe('User visits create item page', () => {
+    describe('posts a new item', () => {
+        it('renders the newly created item', () => {
+            let item = buildItemObject("My favorite item", "https://i.ytimg.com/vi/Ud1wq0lx1oY/hqdefault.jpg", "69 Camaro SS");
+            browser.url('/create.html');
+            browser.setValue('#title-input', item.title);
+            browser.setValue('#description-input', item.description);
+            browser.setValue('#imageUrl-input', item.imageUrl);
+            browser.click('#submit-button');
 
+            assert.include(browser.getText('body'), item.title);
+            assert.include(browser.getAttribute('body img', 'src'), item.imageUrl);
+        })
+    })
+})
