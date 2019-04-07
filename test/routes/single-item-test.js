5e287c169c883e1c014cc1b02f73565420c4785e
SalmonKing72 <madichjm@gmail.com>
2019-04-07 19:27:06 -0400

    server response for single item  red

--- test/routes/single-item-test.js
+++ test/routes/single-item-test.js
@@ -12,5 +12,20 @@ describe('Server path: /items/:id', () => {
   afterEach(diconnectDatabase);
 
   // Write your test blocks below:
-  
+  describe('GET', () => {
+    it('renders a single item and its fields', async () => {
+      const testItem = seedItemToDatabase({
+        description: "My favorite item", 
+        imageUrl: "https://i.ytimg.com/vi/Ud1wq0lx1oY/hqdefault.jpg",
+        title: "69 Camaro SS"
+      });
+
+      const response = await request(app)
+        .get(`/items/${testItem._id}`)
+        .send();
+      
+      assert.include(parseTextFromHTML(response.text, '#item-title'), testItem.title);
+      assert.include(parseTextFromHTML(response.description, '#item-description'), testItem.description);
+    })
+  })
 });
