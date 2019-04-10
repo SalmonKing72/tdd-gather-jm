13f7565286b310213fe8cfa4968515fc2248c396
SalmonKing72 <madichjm@gmail.com>
2019-04-09 22:51:54 -0400

    item update link change, update single item feature test red

--- test/features/user-updates-single-item-test.js
+++ test/features/user-updates-single-item-test.js
@@ -4,15 +4,15 @@ const {buildItemObject} = require('../test-utils');
 describe('User vists create item page.', () => {
     describe('posts a new item.', () => {
         it('updates the item', () => {
-            let itemToDelete = buildItemObject({
+            let itemToUpdate = buildItemObject({
                 description: "An item to update",
                 imageUrl: "https://i.ytimg.com/vi/Ud1wq0lx1oY/hqdefault.jpg",
                 title: "Bad Fruit"
             });
             browser.url('/items/create');
-            browser.setValue('#title-input', itemToDelete.title);
-            browser.setValue('#description-input', itemToDelete.description);
-            browser.setValue('#imageUrl-input', itemToDelete.imageUrl);
+            browser.setValue('#title-input', itemToUpdate.title);
+            browser.setValue('#description-input', itemToUpdate.description);
+            browser.setValue('#imageUrl-input', itemToUpdate.imageUrl);
             browser.click('#submit-button');
 
             browser.click('.item-card a');
