28db7b894d46c71f796e6c7aa9e2bded45efcb69
SalmonKing72 <madichjm@gmail.com>
2019-04-09 21:58:11 -0400

    delete single item feature tests

--- test/features/user-deletes-single-item-test.js
+++ test/features/user-deletes-single-item-test.js
@@ -18,9 +18,10 @@ describe('User vists create item page.', () => {
             browser.moveToObject('.image-overlay').pause(5000);
             let deleteLinkId = browser.element('.delete-form').getAttribute('id');
             let deleteLink = browser.element(`#${deleteLinkId} > p`);
-            console.log(deleteLink.getText());
+            // console.log(deleteLink.getText());
             //attempting to click, via webdriver, the overlay buttons for delete resolves to the single item view...
             // there seems to be a bug with webdriver here... This feature works when you run npm start.
+            // deleteLink.click();
         })
     })
 })
